7f3d1f6b379b892ae2035797db2fda921c75d5e1
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2022-12-29 14:31:52 +0100

    [+] github page setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: process.env.NODE_ENV === "production" ? "/Nathandelenclos/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/Nathandelenclos/'
-      : '/'
 });
