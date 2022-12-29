0fac5390d72d4c9fc794ef676d1e8b01ff782006
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2022-12-29 14:23:35 +0100

    [+] publicPath & serve & app engine file

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/Nathandelenclos/'
+      : '/'
 });
