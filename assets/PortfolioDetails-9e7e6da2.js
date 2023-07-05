08ccb512ae8ba6d158913c4159293d6b18ca6127
Nathandelenclos <Nathandelenclos@users.noreply.github.com>
2023-07-05 18:12:54 +0000

    deploy: 89339c516b2e4835efe1b18a34e319be35f5f588

---
+++ assets/PortfolioDetails-9e7e6da2.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as s,a as e,t as r,F as n}from"./index-72edd92e.js";const l={name:"PortfolioDetails"},c=e("div",{class:"readme"},null,-1);function i(a,p,_,d,f,m){return t(),s(n,null,[e("p",null,r(a.$route.params.id),1),c],64)}const $=o(l,[["render",i]]);export{$ as default};
