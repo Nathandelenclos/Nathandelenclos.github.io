f83dbbe1297dc5a5f45e9d6eb67780f1ca64d685
Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-08-18 17:37:41 +0200

    [~] multiple little update

--- src/views/PortfolioDetails.vue
+++ src/views/PortfolioDetails.vue
@@ -1,16 +1,26 @@
 <template>
-  <p>{{ $route.params.id }}</p>
-  <div class="readme">
-    
+  <div class="readme" v-html="readme">
   </div>
 </template>
 
 <script>
+import repos from "@/assets/repos.json";
+import { marked } from "marked";
+
 export default {
-  name: "PortfolioDetails"
-}
+  name: "PortfolioDetails",
+  data() {
+    return {
+      repo: repos.find((repo) => repo.id === parseInt(this.$route.params.id)),
+      repos,
+      readme: null,
+    };
+  },
+  async mounted() {
+    this.readme = await fetch(`https://raw.githubusercontent.com/${this.repo.full_name}/${this.repo.default_branch}/README.md`).then((res) => res.text());
+    this.readme = marked(this.readme);
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
